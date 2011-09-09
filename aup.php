9dd22d04509c35f586fd951ae03bffb33608cbc2
chrissalij <chris@salij.org>
2011-09-09 22:50:27 +0100

    Fixing indentation.

--- aup.php
+++ aup.php
@@ -19,60 +19,59 @@
 
 				<p>Accessing your account is deemed acceptance of these Terms & Conditions.Please note that Netsoc may terminate your account without notice at any time for any breach of these Terms.</p>
 				<ul>
-				<li>You will be responsible for the content of your pages, including obtaining the legal permission for any and all works which they include, and also ensuring that the contents of these pages does not violate Irish, European or International law. Netsoc shall not be responsible for any illegal or infringing material posted in served websites. Further to this, where a third party confirms illegal activity on a customers served site, Netsoc may at its sole discretion, contact the member immediately, and begin an investigation into same. Any and all services may be suspended while this investigation takes place. Netsoc will make all efforts to conduct any investigation without delay, and will not be responsible or liable for any loss, damage, whether general or consequential caused by same. Where Netsoc considers material on the site as illegal, defamatory or abusive they shall, at their sole discretion terminate services.</li>
-				<li> You're also bound by UCD's <a href="http://www.ucd.ie/itservices/itsupport/acceptableusage/#d.en.70461">acceptable usage policy.</a></li>
-<li>Your server (inc FTP and Shell access) is for your personal use only. You must not divulge the password to any other person, and you should take reasonable precautions to ensure that it is not discovered by other people. If this is compromised you must take your own appropriate action.
-Data stored on our servers is not guaranteed to be backed up. It is recommended that you keep an independent backup of all data stored on your virtual server or shared hosting account. Netsoc accept no responsibility for any loss of data, however incurred.</li>
-<li>It is the Users sole responsibility to keep all files, date, passwords, etc, secure at all times. Further to this, any security applications, or security details of the user, must be kept at the users own risk, and kept up to date, so that protection is afforded for both the User and the Society. The Society will not be held responsible for damage, loss or injury caused, due to insufficient or out of date security measures held by the User.</li>
-<li>Netsoc reserve the right to remove any cgi or similar server-side program that interferes in any way with the normal operation of our servers and services.</li>
-<li>Known dangerous scripts, such as Matt's formmail are not allowed.</li>
-<li>We shall not be held liable for any loss or damages caused by the use or misuse, unavailability or removal of services.</li>
-<li>When your account is closed, all files (including web pages, etc.) will be deleted, it is your responsibility to ensure all your data is backed up.</li>
-<li>We reserve the right to cancel your account at any time without notice.</li>
-<li>We reserve the right to amend and update these Terms and Conditions at any time without notice.</li>
-<li>To protect your privacy, we will not distribute your name, email address, domain name, unless required to do so under law, or where same is required in relation to the successful provision of services. The list of information which may be provided under this condition is not exhaustive. When such a request for information on a member is made Netsoc may at it.s discretion inform said member.</li>
-<li>Users must not participate in any form of un-solicited bulk e-mailing or Spam, or any form of illegal web site. Failure to observe this condition will result in immediate disconnection of service (see below).</li>
-</ul>
-<h3>Email Abuse - Spam / UCE</h3>
-<ul>
-<li>Email Abuse and Email Misuse is a serious problem, and Netsoc will not tolerate it.</li>
-<li>Definition of UCE (Unsolicited Commercial Email), or SPAM:</li>
-<li>The bulk UCE, bulk promotional material, or other forms of solicitation sent via Email that advertise any IP address belonging to Netsoc. or any URL (domain) that is hosted by Netsoc.</li>
-<li>Unsolicited postings to newsgroups advertising any IP or URL hosted by Netsoc.</li>
-<li>The use of web pages set up on ISPs that allow SPAM-ing (also known as "ghost sites") that directly or indirectly reference customers to domains or IP addresses hosted by Netsoc.</li>
-<li>Advertising, transmitting, or otherwise making available any software, program, product, or service that is designed to facilitate a means to SPAM.</li>
-<li>Forging or misrepresenting message headers, whether in whole or in part, to mask the true origin of the message.</li>
-<li>For further information on mail abuse, please visit the Mail Abuse Prevention System (MAPS) website.</li>
-<li>Repercussions of SPAM:</li>
-<li>Across the Web, it is generally accepted that SPAM is an inconsiderate and improper business practice.</li>
-<li>Punishment for SPAM:</li>
-<li>Netsoc reserves the right to terminate, without warning, any account that violates this policy.</li>
-<li>Usage of Netsoc services constitutes acceptance and understanding of this policy.</li>
-</ul>
-<h3>Bandwidth / Resource Usage</h3>
-<ul>
-<li>If your resource usage reaches the points where it has an adverse affect on other members we reserve the right to disable your site until you can reduce your bandwidth / resource usage.</li>
-</ul>
-<h4>Web pages</h4>
-<ul>
-<li>Commercial use of web and ftp space is not permitted.</li>
-<li>You will be responsible for the content of your pages, including obtaining the legal permission for any works they include and ensuring that the contents of these pages do not violate Irish or European law and the Netsoc shall not take responsibility for same.</li>
-<li>You will be held responsible for and accept responsibility for any defamatory, confidential, secret or other proprietary material available via your page(s).</li>
-<li>We reserve the right to remove material deemed inappropriate from your web pages, without prior notice.</li>
-</ul>
-<h3>Glossary of Terms</h3>
-<ul>
-<li>Warez: Widely used in cracker subcultures to denote cracked versions of commercial software, that is versions from which copy-protection has been stripped</li>
-<li>IRC Bots: An automated program that operates on IRC.</li>
-<li>IRC: A client-server chat system of large (often worldwide) networks</li>
-<li>Shell Hosting: A hosting account used primarily for either shell commands or to run shell processes, such as IRC bouncers etc.</li>
-<li>Bouncer (bnc): a bnc is short for 'bouncer'. A bnc acts as a proxy to IRC, allowing you to hide your real IP address and use a vhost (vanity host - something like 'this.is.high.quality.web.hosting.in.ireland.com').</li>
-<li>Cgi: A standard for running external programs from a World-Wide Web HTTP server. CGI specifies how to pass arguments to the executing program as part of the HTTP request. It also defines a set of environment variables. Commonly, the program will generate some HTML which will be passed back to the browser but it can also request URL redirection</li>
-<li>Shell: The command interpreter used to pass commands to an operating system; so called because it is the part of the operating system that interfaces with the outside world. The commonest Unix shells are the c shell (csh) and the Bourne shell (sh).</li>
-<li>FTP: A communications protocol governing the transfer of files from one computer to another over a network</li>
-<li>DNS: A general-purpose distributed, replicated, data query service chiefly used on Internet for translating hostnames into Internet addresses. Also, the style of hostname used on the Internet, though such a name is properly called a fully qualified domain name. DNS can be configured to use a sequence of name servers, based on the domains in the name being looked for, until a match is found</li>
-<li>bash_history - .bash_logout - .bash_profile - .bashrc..: A number of files related to a shell account. These include configuration directives and logging of user actions.</li>
-</ul>
+					<li>You will be responsible for the content of your pages, including obtaining the legal permission for any and all works which they include, and also ensuring that the contents of these pages does not violate Irish, European or International law. Netsoc shall not be responsible for any illegal or infringing material posted in served websites. Further to this, where a third party confirms illegal activity on a customers served site, Netsoc may at its sole discretion, contact the member immediately, and begin an investigation into same. Any and all services may be suspended while this investigation takes place. Netsoc will make all efforts to conduct any investigation without delay, and will not be responsible or liable for any loss, damage, whether general or consequential caused by same. Where Netsoc considers material on the site as illegal, defamatory or abusive they shall, at their sole discretion terminate services.</li>
+					<li> You're also bound by UCD's <a href="http://www.ucd.ie/itservices/itsupport/acceptableusage/#d.en.70461">acceptable usage policy.</a></li>
+					<li>Your server (inc FTP and Shell access) is for your personal use only. You must not divulge the password to any other person, and you should take reasonable precautions to ensure that it is not discovered by other people. If this is compromised you must take your own appropriate action. Data stored on our servers is not guaranteed to be backed up. It is recommended that you keep an independent backup of all data stored on your virtual server or shared hosting account. Netsoc accept no responsibility for any loss of data, however incurred.</li>
+					<li>It is the Users sole responsibility to keep all files, date, passwords, etc, secure at all times. Further to this, any security applications, or security details of the user, must be kept at the users own risk, and kept up to date, so that protection is afforded for both the User and the Society. The Society will not be held responsible for damage, loss or injury caused, due to insufficient or out of date security measures held by the User.</li>
+					<li>Netsoc reserve the right to remove any cgi or similar server-side program that interferes in any way with the normal operation of our servers and services.</li>
+					<li>Known dangerous scripts, such as Matt's formmail are not allowed.</li>
+					<li>We shall not be held liable for any loss or damages caused by the use or misuse, unavailability or removal of services.</li>
+					<li>When your account is closed, all files (including web pages, etc.) will be deleted, it is your responsibility to ensure all your data is backed up.</li>
+					<li>We reserve the right to cancel your account at any time without notice.</li>
+					<li>We reserve the right to amend and update these Terms and Conditions at any time without notice.</li>
+					<li>To protect your privacy, we will not distribute your name, email address, domain name, unless required to do so under law, or where same is required in relation to the successful provision of services. The list of information which may be provided under this condition is not exhaustive. When such a request for information on a member is made Netsoc may at it.s discretion inform said member.</li>
+					<li>Users must not participate in any form of un-solicited bulk e-mailing or Spam, or any form of illegal web site. Failure to observe this condition will result in immediate disconnection of service (see below).</li>
+				</ul>
+				<h3>Email Abuse - Spam / UCE</h3>
+				<ul>
+					<li>Email Abuse and Email Misuse is a serious problem, and Netsoc will not tolerate it.</li>
+					<li>Definition of UCE (Unsolicited Commercial Email), or SPAM:</li>
+					<li>The bulk UCE, bulk promotional material, or other forms of solicitation sent via Email that advertise any IP address belonging to Netsoc. or any URL (domain) that is hosted by Netsoc.</li>
+					<li>Unsolicited postings to newsgroups advertising any IP or URL hosted by Netsoc.</li>
+					<li>The use of web pages set up on ISPs that allow SPAM-ing (also known as "ghost sites") that directly or indirectly reference customers to domains or IP addresses hosted by Netsoc.</li>
+					<li>Advertising, transmitting, or otherwise making available any software, program, product, or service that is designed to facilitate a means to SPAM.</li>
+					<li>Forging or misrepresenting message headers, whether in whole or in part, to mask the true origin of the message.</li>
+					<li>For further information on mail abuse, please visit the Mail Abuse Prevention System (MAPS) website.</li>
+					<li>Repercussions of SPAM:</li>
+					<li>Across the Web, it is generally accepted that SPAM is an inconsiderate and improper business practice.</li>
+					<li>Punishment for SPAM:</li>
+					<li>Netsoc reserves the right to terminate, without warning, any account that violates this policy.</li>
+					<li>Usage of Netsoc services constitutes acceptance and understanding of this policy.</li>
+				</ul>
+				<h3>Bandwidth / Resource Usage</h3>
+				<ul>
+					<li>If your resource usage reaches the points where it has an adverse affect on other members we reserve the right to disable your site until you can reduce your bandwidth / resource usage.</li>
+				</ul>
+				<h4>Web pages</h4>
+				<ul>
+					<li>Commercial use of web and ftp space is not permitted.</li>
+					<li>You will be responsible for the content of your pages, including obtaining the legal permission for any works they include and ensuring that the contents of these pages do not violate Irish or European law and the Netsoc shall not take responsibility for same.</li>
+					<li>You will be held responsible for and accept responsibility for any defamatory, confidential, secret or other proprietary material available via your page(s).</li>
+					<li>We reserve the right to remove material deemed inappropriate from your web pages, without prior notice.</li>
+				</ul>
+				<h3>Glossary of Terms</h3>
+				<ul>
+					<li>Warez: Widely used in cracker subcultures to denote cracked versions of commercial software, that is versions from which copy-protection has been stripped</li>
+					<li>IRC Bots: An automated program that operates on IRC.</li>
+					<li>IRC: A client-server chat system of large (often worldwide) networks</li>
+					<li>Shell Hosting: A hosting account used primarily for either shell commands or to run shell processes, such as IRC bouncers etc.</li>
+					<li>Bouncer (bnc): a bnc is short for 'bouncer'. A bnc acts as a proxy to IRC, allowing you to hide your real IP address and use a vhost (vanity host - something like 'this.is.high.quality.web.hosting.in.ireland.com').</li>
+					<li>Cgi: A standard for running external programs from a World-Wide Web HTTP server. CGI specifies how to pass arguments to the executing program as part of the HTTP request. It also defines a set of environment variables. Commonly, the program will generate some HTML which will be passed back to the browser but it can also request URL redirection</li>
+					<li>Shell: The command interpreter used to pass commands to an operating system; so called because it is the part of the operating system that interfaces with the outside world. The commonest Unix shells are the c shell (csh) and the Bourne shell (sh).</li>
+					<li>FTP: A communications protocol governing the transfer of files from one computer to another over a network</li>
+					<li>DNS: A general-purpose distributed, replicated, data query service chiefly used on Internet for translating hostnames into Internet addresses. Also, the style of hostname used on the Internet, though such a name is properly called a fully qualified domain name. DNS can be configured to use a sequence of name servers, based on the domains in the name being looked for, until a match is found</li>
+					<li>bash_history - .bash_logout - .bash_profile - .bashrc..: A number of files related to a shell account. These include configuration directives and logging of user actions.</li>
+				</ul>
 			</div>
 			<?php include("footer.php"); ?>
 		</div>

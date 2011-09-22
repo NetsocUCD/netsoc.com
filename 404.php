896a25a00e6eb33ec3107610dde90250e8c9f7ce
Aidan Church <aidan@Cookiemonster>
2011-09-22 23:10:46 +0100

    Changed the video

--- 404.php
+++ 404.php
@@ -4,8 +4,7 @@
 			<?php include("header.php"); ?>
 			<div id="content">
 				<h1>404 | File not found</h1>
-				<iframe width="420" height="315" src="http://www.youtube-nocookie.com/embed/4EYlXay0njE?rel=0" frameborder="0" allowfullscreen></iframe>
-				
+				<iframe width="420" height="315" style="text-align:center" src="http://www.youtube-nocookie.com/embed/4EYlXay0njE?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
 			</div>
 			<?php include("footer.php"); ?>
 		</div>

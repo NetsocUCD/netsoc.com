1265e40e976af39435ef97816a0f3d9f49436fad
Aidan Church <aidan@Cookiemonster>
2011-09-22 23:06:40 +0100

    Added the 404 page.

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php include("head.php"); ?>
+	<body>
+		<div id="wrapper">
+			<?php include("header.php"); ?>
+			<div id="content">
+				<h1>404 | File not found</h1>
+				<iframe width="420" height="315" src="http://www.youtube-nocookie.com/embed/4EYlXay0njE?rel=0" frameborder="0" allowfullscreen></iframe>
+				
+			</div>
+			<?php include("footer.php"); ?>
+		</div>
+	</body>
+</html>

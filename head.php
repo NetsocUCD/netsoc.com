e77782f747ddd10204c84c0a81bfe7f3dd2a9574
Sam Dunne <sam.dunne101@gmail.com>
2011-11-08 14:56:35 +0000

    Refined G+
    Added a +1 button to footer along with G+ page button

--- head.php
+++ head.php
@@ -8,16 +8,22 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
 		<link href="https://plus.google.com/117765251083744498766/" rel="publisher" />
 		<script type="text/javascript">
+ 			var _gaq = _gaq || [];
+  			_gaq.push(['_setAccount', 'UA-12995041-2']);
+ 			_gaq.push(['_trackPageview']);
+  			(function() {
+    				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  			})();
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-12995041-2']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
+		</script>
+		<script type="text/javascript">
+ 	 		window.___gcfg = {lang: 'en-GB'};
+			(function() {
+    				var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    				po.src = 'https://apis.google.com/js/plusone.js';
+    				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  			})();
+		</script>
 	</head>

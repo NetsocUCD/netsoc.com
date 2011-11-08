bbcedd66db453ac8cc478ea227ef5ef3c7ba2c32
Sam Dunne <sam.dunne101@gmail.com>
2011-11-08 14:59:03 +0000

    Fixed more formatting problems

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 			<!-- The websites footer -->
 			<div id="footer">
+				<p>
 				<a href="https://plus.google.com/117765251083744498766/?prsrc=3" style="text-decoration: none;">
 				<img src="https://ssl.gstatic.com/images/icons/gplus-16.png" width="16" height="16" style="border: 0;"/></a>
-				<p>Site design / logo &copy; Netsoc 2011. Third party contributions licensed under <a href="http://creativecommons.org/licenses/by-sa/3.0/"> cc.wiki</a>.<p>
-				<g:plusone size="small" annotation="inline"></g:plusone>
+				|| Site design / logo &copy; Netsoc 2011. Third party contributions licensed under <a href="http://creativecommons.org/licenses/by-sa/3.0/"> cc.wiki</a>. ||						   <g:plusone size="small" annotation="inline"></g:plusone>
+				</p>
 			</div>

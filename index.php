1dc47d017232c1275080b729c957bc6a01907cd9
Sam Dunne <sam.dunne101@gmail.com>
2012-03-16 11:53:57 +0000

    Rest in Peace Paul

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 		<div id="wrapper">
 			<?php include("header.php"); ?>
             <div id="content">
+                <h1>Paul Bunbury</h1>
+                <p align="center">UCD Netsoc would like to extend their deepest sympathies to the family and friends of Paul Bunbury. We wish them all the best in their time of grieving.<br /><br />
+                    Rest in Peace Paul.<br />
+                    <center><img src="http://www.redbrick.dcu.ie/includes/images/uploads/articles/16/bunbunupdate.png"></img></center>
+                </p>
+
                 <h1>Diaspora</h1>
                 <p> UCD Netsoc has just set up their own <a href="http://diasporafoundation.org/">Diaspora pod</a>. We are the first diaspora pod in Ireland and would like everyone to use it.<br />
                     <a href="http://diaspora.netsoc.com:3000">Join here!</a>
